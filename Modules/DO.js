310f4118cbc10b2f4c53fcada74ffacd3b02f2e8
Mohammad Sadegh Foroughi <mohamad.foroughilam@gmail.com>
2020-05-09 05:22:33 +0430

    add routes and DataObject

---
+++ Modules/DO.js
@@ -0,0 +1,53 @@
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
+
+function DO() {
+  this.DOpath = "db/DO";
+  this.initContent = `[]`;
+  this.DataObject = null;
+
+  this.getDO = () => {
+    let tempDo = fs.readFileSync(this.DOpath, "utf8");
+    this.DataObject = JSON.parse(tempDo);
+  };
+
+  this.init = () => {
+    if (!fs.existsSync(this.DOpath)) {
+      var createStream = fs.createWriteStream(this.DOpath);
+      createStream.end();
+      fs.writeFile(this.DOpath, this.initContent, (err) => {
+        if (err) throw err;
+        console.log("The DO was succesfully created!");
+        this.getDO();
+      });
+    } else {
+      this.getDO();
+    }
+  };
+
+  this.read = () => {
+    return this.DataObject;
+  };
+
+  this.insert = (obj) => {
+    let tempDO = this.DataObject;
+    !obj.id && uuidv4();
+    tempDO.push(obj);
+    fs.writeFileSync(this.DOpath, JSON.stringify(tempDO));
+    this.getDO();
+  };
+
+  this.remove = (id) => {
+    let tempDO = this.DataObject;
+    tempDO = tempDO.filter((i) => i.id != id);
+    fs.writeFileSync(this.DOpath, JSON.stringify(tempDO));
+    this.getDO()
+  };
+
+  this.update = (id, obj) => {
+    this.remove(id);
+    this.insert(obj);
+  };
+}
+
+module.exports = DO;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,30 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const DO = require("../Modules/DO");
+const DataObject = new DO();
+DataObject.init();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.post("/create", function (req, res, next) {
+  DataObject.insert(req.body);
+  res.json(DataObject.read());
+});
+
+router.get("/read", function (req, res, next) {
+  res.json(DataObject.read());
+});
+
+router.post("/update", function (req, res, next) {
+  DataObject.update(req.body.id, req.body);
+  res.json(DataObject.read());
+});
+
+router.post("/delete", function (req, res, next) {
+  DataObject.remove(req.body.id);
+  res.json(DataObject.read());
 });
 
 module.exports = router;
